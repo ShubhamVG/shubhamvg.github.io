7f9579ed610559520cc19397ed908af8ed695407
ShubhamVG <vgshubham49@gmail.com>
2025-02-13 18:27:41 +0530

    Add navbar

--- web/main.dart
+++ web/main.dart
@@ -1,18 +1,31 @@
 import 'dart:async';
+import 'dart:js_interop';
 
 import 'package:web/web.dart' as web;
 
 void main() {
-  final typewritter = web.document.getElementById('typewritter-text')
-      as web.HTMLParagraphElement;
-  _startTypewritter(typewritter);
+  _addListeners();
+  _startTypewritter();
 }
 
-void _startTypewritter(final web.HTMLParagraphElement typewritter) {
+void _addListeners() {
+  final hamMenu = web.document.getElementById('ham-menu') as web.HTMLDivElement;
+  final navItems =
+      web.document.getElementById('nav-items') as web.HTMLDivElement;
+
+  hamMenu.onclick = () {
+    hamMenu.classList.toggle('active');
+    navItems.classList.toggle('active');
+  }.toJS;
+}
+
+void _startTypewritter() {
+  final typewritter = web.document.getElementById('typewritter-text')
+      as web.HTMLParagraphElement;
   const texts = <String>[
-    'this is a very long sentence',
-    'but this sentence could be longer',
-    'honestly, no one cares',
+    'Interested to know me?',
+    'Or, maybe in my stuff',
+    'Thanks for dropping by!',
   ];
   int textIdx = 0;
 
