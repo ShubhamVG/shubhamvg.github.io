fd33aa3dbae22eeffab9e2e5ce8e86a7cdfd130c
ShubhamVG <vgshubham49@gmail.com>
2025-02-23 23:01:21 +0530

    Add css colors

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -1,5 +1,7 @@
 import 'package:jaspr/jaspr.dart';
 
+import '../utils/colors.dart';
+
 final ballBg1 = 'radial-gradient(circle, hsla(147, 100%, 36%, 1) 0%,'
     'hsla(194, 83%, 26%, 0) 50%)';
 
@@ -13,6 +15,17 @@ final ballSize = 80.percent;
 
 final bgGradient = 'linear-gradient(135deg, rgb(8, 43, 24) 0%, rgb(15, 58, 34)'
     '26%, hsla(147, 50%, 8%, 1) 60%, hsla(0, 0%, 0%, 1) 100%)';
+
+// final darkGreen = Color.rgba(0x05, 0x50, 0x2b, 0x9c / 0xff);
+final darkGreen = hexAlpha('#05502b9c');
+
+final hamMenuColor = Color.hex('#86ffc1');
+
+final navBgActiveColor = hexAlpha('#07633559');
+
+final shinyGreen = Color.rgb(0, 141, 59);
+
+final textShadowColor = hexAlpha('#00ffaa80');
 
 final titliWebUrl = 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,'
     'wght@0,14..32,100..900;1,14..32,100..900&family=Josefin+Sans:ital,wght@0,'
