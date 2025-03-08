560c18e7f6899772da393d34a8e0c68e46e27e7e
ShubhamVG <vgshubham49@gmail.com>
2025-03-08 22:29:08 +0530

    Markdownify home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_router/jaspr_router.dart';
 
 import '../components/main_.dart';
+import '../components/markdown_text.dart';
 import '../components/typewriter.dart';
 import '../constants/routes.dart';
 import '../utils/interfaces.dart';
@@ -13,7 +14,7 @@ class Home extends StatelessComponent implements IPage {
   Route get route {
     return Route(
       path: '/',
-      title: 'LaV',
+      title: "LaV's Digital Home",
       builder: (_, __) => this,
     );
   }
@@ -32,33 +33,25 @@ class Home extends StatelessComponent implements IPage {
           'Or, maybe in my stuff',
           'Thanks for dropping by!',
         ]),
-        section([
-          h1([const Text("Welcome to LaV's Digital Home")]),
-          p([
-            const RawText(
-              "First, let me tell you about myself so I go by LaV and I am a 20 "
-              "y/o lad who like to build stuff (<strong>not</strong> limited to "
-              "coding)."
-              "<br>"
-              "Previously, I used to tell everyone that I am a problem solver "
-              "but lately, I haven't came across any \"problem\" to solve so "
-              "here I am, building beautiful stuff like this website!"
-              "<br><br>"
-              "You can check out my projects on the "
-              '<a href="/projects/" class="badge">Project</a> page or on my '
-              '<a href="https://github.com/ShubhamVG/" class="wavy-text">GitHub'
-              '</a> if it is open-sourced.'
-              '<br>'
-              '(Do drop me a follow and star the repos that you like ;)'
-              '<br>'
-              '<br>'
-              "Also, checkout my<a href='$cvLink' class='fancy-badge' target='_blank'>"
-              "<span>Resume<span></a>"
-              "'cuz why not?",
-            ),
-          ]),
-        ]),
+        const MarkdownText(_markdown),
       ],
     );
   }
 }
+
+const _markdown = """
+# Welcome to LaV's Digital Home
+First, let me tell you about myself so I go by LaV and I am a 20 y/o lad who 
+likes to build stuff (**not** limited to coding).<br>
+Previously, I used to tell everyone that I am a problem solver but lately, I 
+haven't came across any "problem" to solve so here I am, building beautiful 
+stuff like this website!
+
+You can check out my projects on the <a href="/projects/" class="badge">Project
+</a> page or on my <a href="https://github.com/ShubhamVG/" class="wavy-text">
+GitHub</a> if it is open-sourced.<br>
+(Do drop me a follow and star the repos that you like ;)
+
+Also, checkout my <a href='$cvLink' class='fancy-badge' target='_blank'><span>
+Resume<span></a> 'cuz why not?
+""";
